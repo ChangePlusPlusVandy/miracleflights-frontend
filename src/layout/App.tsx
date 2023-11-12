2efd0706ba4fd827d44ba045f108d16e46586f63
jacoblurie29 <jacoblurie29@gmail.com>
2023-11-12 15:01:21 -0600

    APP 26 I added a comment

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   // App wrapper for tabs
   console.log("App");
 
-  // Put the sidebar in here!
+  // The side bar should go in here!
 
   return <div className={styles.appContainer}>Hello, world!</div>;
 };
