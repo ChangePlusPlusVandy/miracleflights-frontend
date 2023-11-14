bfe0d9de3fedbf0a34fa252f95474e365c7c3ce3
Jacob Davey <jacob.j.davey@example.com>
2023-11-13 19:06:14 -0600

    APP 23 Fixed tab package

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,8 +1,40 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 const PersonalInfo = () => {
   // Personal info tab
   console.log("PersonalInfo");
 
-  return <div>PersonalInfo</div>;
+  return (
+    <div>
+      Personal Info
+      <Tabs>
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+          <Tab>Four</Tab>
+          <Tab>Five</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Fuck u jake</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default PersonalInfo;
