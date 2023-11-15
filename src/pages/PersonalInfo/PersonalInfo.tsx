71985916fb3f90c7dd391b618eb61ac7a1bd8a88
Jacob Davey <jacob.j.davey@example.com>
2023-11-14 23:24:06 -0600

    APP 23 Made the tab bar

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,39 +1,32 @@
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+import { useState, useEffect } from 'react';
+import styles from './PersonalInfo.module.css'
+import inter from './'
 
 const PersonalInfo = () => {
   // Personal info tab
   console.log("PersonalInfo");
+  const [tabIndex, setTabIndex] = useState(0);
+  const tabs = ["One", "Two", "Three", "Four", "Five"];
 
   return (
-    <div>
-      Personal Info
-      <Tabs>
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-          <Tab>Four</Tab>
-          <Tab>Five</Tab>
-        </TabList>
+    <div className={styles.tabs}>
+      <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Inter"></link>
+      <div className={styles.tabList}>
+        {tabs.map((value, index) => (
+          <button type="button" className={index===tabIndex-1? styles.tabSelected: styles.tab} onClick={() => {
+            setTabIndex(index + 1)
+          }}>{value}</button>
+        ))}
+      </div>
 
-        <TabPanel>
-          <h2>Info 1</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 1</h2>
-        </TabPanel>
-      </Tabs>
+      {tabIndex === 1 && <div className={styles.tabPanel}>ONE</div>}
+      {tabIndex === 2 && <div className={styles.tabPanel}>TWO</div>}
+      {tabIndex === 3 && <div className={styles.tabPanel}>THREE</div>}
+      {tabIndex === 4 && <div className={styles.tabPanel}>FOUR</div>}
+      {tabIndex === 5 && <div className={styles.tabPanel}>FIVE</div>}
     </div>
+
   );
 };
 
