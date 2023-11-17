eb5caae48296fedc8c42de6c8c4313a35e366cd3
Jacob Davey <jacob.j.davey@example.com>
2023-11-16 19:51:36 -0600

    APP 23 Added animations and fixed styling

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,32 +1,34 @@
-import { useState, useEffect } from 'react';
-import styles from './PersonalInfo.module.css'
-import inter from './'
+import styles from "./PersonalInfo.module.css";
+import { useState } from "react";
 
 const PersonalInfo = () => {
-  // Personal info tab
-  console.log("PersonalInfo");
   const [tabIndex, setTabIndex] = useState(0);
   const tabs = ["One", "Two", "Three", "Four", "Five"];
 
   return (
     <div className={styles.tabs}>
-      <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Inter"></link>
+
       <div className={styles.tabList}>
         {tabs.map((value, index) => (
-          <button type="button" className={index===tabIndex-1? styles.tabSelected: styles.tab} onClick={() => {
-            setTabIndex(index + 1)
-          }}>{value}</button>
+          <button
+            key={value}
+            type="button"
+            className={index === tabIndex ? styles.tabSelected : styles.tab}
+            onClick={() => {
+              setTabIndex(index);
+            }}
+          >
+            {value}
+          </button>
         ))}
       </div>
 
-      {tabIndex === 1 && <div className={styles.tabPanel}>ONE</div>}
-      {tabIndex === 2 && <div className={styles.tabPanel}>TWO</div>}
-      {tabIndex === 3 && <div className={styles.tabPanel}>THREE</div>}
-      {tabIndex === 4 && <div className={styles.tabPanel}>FOUR</div>}
-      {tabIndex === 5 && <div className={styles.tabPanel}>FIVE</div>}
+      {tabIndex === 0 && <div className={styles.tabPanel}>ONE</div>}
+      {tabIndex === 1 && <div className={styles.tabPanel}>TWO</div>}
+      {tabIndex === 2 && <div className={styles.tabPanel}>THREE</div>}
+      {tabIndex === 3 && <div className={styles.tabPanel}>FOUR</div>}
+      {tabIndex === 4 && <div className={styles.tabPanel}>FIVE</div>}
     </div>
-
   );
 };
 
