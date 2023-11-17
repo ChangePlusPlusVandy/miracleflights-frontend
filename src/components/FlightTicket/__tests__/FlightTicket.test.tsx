2547ad938a756ecb930fc1c2d318d0f176342a12
vab5 <varun.bussa@vanderbilt.edu>
2023-11-17 12:00:19 -0600

    finished testing

--- src/components/FlightTicket/__tests__/FlightTicket.test.tsx
+++ src/components/FlightTicket/__tests__/FlightTicket.test.tsx
@@ -85,7 +85,42 @@ describe("FlightTicket Tests", () => {
         airline={"SOUTHWEST"}
         legType={LegType.RETURN}
         colorVariant={FlightTicketColorVariant.BLUE}
-        isLastElement={false}
+        isLastElement={true}
+      />,
+    );
+
+    // Check that the component renders
+    expect(component).toBeTruthy();
+
+    //check that date is the same
+    const date = component.getByText("11/23/2023");
+    expect(date).toBeTruthy();
+
+    //check that the departing airport is the same
+    const departingAirport = component.getByText("LAX");
+    expect(departingAirport).toBeTruthy();
+
+    const arrivingAirport = component.getByText("BNA");
+    expect(arrivingAirport).toBeTruthy();
+
+    const airline = component.getByText("SOUTHWEST");
+    expect(airline).toBeTruthy();
+
+    const legType = component.getByText("RETURN");
+    expect(legType).toBeTruthy();
+  });
+
+  /**************************Fourth TEST*******************************/
+  test("Test example component with connecting flight and red color", () => {
+    const component = render(
+      <FlightTicket
+        date={"11/23/2023"}
+        departingAirport={"LAX"}
+        arrivingAirport={"BNA"}
+        airline={"SOUTHWEST"}
+        legType={LegType.RETURN}
+        colorVariant={FlightTicketColorVariant.RED}
+        isLastElement={true}
       />,
     );
 
