c480971d6a44e1f309cb4981074aebf2eeaab53d
Emre Bilge <105619318+emrebilge@users.noreply.github.com>
2023-11-18 01:26:43 -0500

    Adding Some Features

--- src/components/ButtonComponent/ButtonComponent.tsx
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -1,5 +1,16 @@
-import styles from "./ButtonComponent.module.css";
-import { useState } from "react";
-import type { ButtonComponentProps } from "./ButtonComponent.definitions";
+// src/components/Button/Button.tsx
+import styles from './ButtonComponent.module.css';
+// import React from 'react';
+import type { ButtonProps } from './ButtonComponent.definitions';
 
+const Button = ({ variant, color, onClick, text, loading }: ButtonProps) => {
+    const buttonClassName = `${styles.button} ${styles[variant]} ${styles[color]}`;
 
+    return (
+        <button className={buttonClassName} onClick={onClick} disabled={loading}>
+            {loading ? 'Loading...' : text}
+        </button>
+    );
+};
+
+export default Button;
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,14 +1,8 @@
 export enum ButtonColors {
-    BLUE = "Blue",
-    YELLOW = "Yellow",
-    GREEN = "Green",
-    GREY = "Grey",
-    RED = "Red",
-    BLACK = "Black"
-}
-
-export enum ButtonVariants {
-  COMPACT = 'Compact',
-  REGULAR = 'Regular',
-  LARGE = 'Large',
+  Blue = 'Blue',
+  Yellow = 'Yellow',
+  Green = 'Green',
+  Grey = 'Grey',
+  Red = 'Red',
+  Black = 'Black',
 }
