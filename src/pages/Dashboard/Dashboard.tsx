cef6e90d504bd2470991015f134f6536de25cd9a
mahdmohsin54 <mahdmohsin54@gmail.com>
2023-11-19 17:47:15 -0600

    Changes to reusableInput  for review

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,37 @@
+import ReusableInput from "./inputComponent";
+import { useForm } from "react-hook-form";
+
 const Dashboard = () => {
   // Dashboard tab
   console.log("Dashboard");
 
-  return <div>Dashboard</div>;
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ReusableInput
+          name="Email"
+          register={register}
+          errors={errors}
+          isRequired={true}
+          label="Email"
+          placeholder="Email"
+          defaultValue=""
+          errorText="(Required)"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/inputComponent.definitions.ts
@@ -0,0 +1,10 @@
+export interface ReusableInputProps {
+  name: string;
+  register: any;
+  errors: any;
+  isRequired: boolean;
+  label: string;
+  placeholder: string;
+  defaultValue: string;
+  errorText: string;
+}
