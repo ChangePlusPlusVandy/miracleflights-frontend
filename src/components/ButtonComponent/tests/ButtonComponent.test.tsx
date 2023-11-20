b1d4a62e7fbb17ce3aaefc6520ec5fbffd5bbdf2
Emre Bilge <105619318+emrebilge@users.noreply.github.com>
2023-11-20 15:47:25 -0500

    Adding spinning feature

--- src/components/ButtonComponent/tests/ButtonComponent.test.tsx
+++ src/components/ButtonComponent/tests/ButtonComponent.test.tsx
@@ -2,14 +2,15 @@ import ButtonComponent from "../ButtonComponent";
 import { ButtonColors } from "../../../constants/constants";
 import { ButtonVariant } from "../../../constants/constants";
 import { render } from "@testing-library/react";
-import React from "react";
 const handleButtonClick = () => {
-    console.log('Button clicked!');
-  };
+  console.log('Button clicked!');
+};
 
 describe("ButtonComponent Tests", () => {
     /**************************FIRST TEST*******************************/
     test("Test Button component to render", () => {
+  
+
         const component = render(<ButtonComponent
             variant={ButtonVariant.Regular}
             color={ButtonColors.Blue}
@@ -19,10 +20,5 @@ describe("ButtonComponent Tests", () => {
           />);
       // Check that the component renders
       expect(component).toBeTruthy();
-      const subText = component.getByText(
-          "Test Button",
-        );
-      expect(subText).toBeTruthy();
-  
   });
 });
