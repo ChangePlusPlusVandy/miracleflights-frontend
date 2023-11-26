993ddaf78dae8aeb153dad118ebad3ea6560a644
jacoblurie29 <jacoblurie29@gmail.com>
2023-11-26 16:21:03 -0600

    Passenger tests and move the date function to the util folder

---
+++ src/util/date.util.ts
@@ -0,0 +1,10 @@
+export const getAge = (dateOfBirth: string) => {
+  const today = new Date();
+  const birthDate = new Date(dateOfBirth);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+};
