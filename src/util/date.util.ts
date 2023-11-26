b2ac3516814bc99953a90e19d14246a0fa919f85
jacoblurie29 <jacoblurie29@gmail.com>
2023-11-26 16:30:36 -0600

    Commented the age function and added some tests

--- src/util/date.util.ts
+++ src/util/date.util.ts
@@ -1,10 +1,29 @@
 export const getAge = (dateOfBirth: string) => {
   const today = new Date();
+
+  // Convert dateOfBirth to Date object
   const birthDate = new Date(dateOfBirth);
+
+  // Calculate age
   let age = today.getFullYear() - birthDate.getFullYear();
+
+  // Calculate month difference
   const m = today.getMonth() - birthDate.getMonth();
+
+  // If today's month is less than the birth month, subtract 1 from age
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
+
+  // If age is negative, return 0
+  if (age < 0) {
+    return 0;
+  }
+
+  // If age is NaN, return 0. This happens when dateOfBirth is not a valid date
+  if (isNaN(age)) {
+    return 0;
+  }
+
   return age;
 };
