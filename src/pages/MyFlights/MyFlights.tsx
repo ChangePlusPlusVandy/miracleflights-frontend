fbc25e16f0d09b044d8a5828051faa011ab62592
vab5 <varun.bussa@vanderbilt.edu>
2023-11-25 22:54:15 -0600

    add z-index to modal background to make sure it renders above all parent components if there are multiple

--- src/pages/MyFlights/MyFlights.tsx
+++ src/pages/MyFlights/MyFlights.tsx
@@ -11,13 +11,22 @@ const MyFlights = () => {
   return (
     <div>
       <FlightTicket
-        legType={LegType.CONNECTING}
+        legType={LegType.DEPARTURE}
         date="11/14/2023"
-        departingAirport="BNA "
+        departingAirport="BNA"
         arrivingAirport="JFK"
         airline="Southwest"
         colorVariant={FlightTicketColorVariant.BLUE}
         isLastElement={false}
+      />
+      <FlightTicket
+        legType={LegType.RETURN}
+        date="11/15/2023"
+        departingAirport="JFK"
+        arrivingAirport="BNA"
+        airline="Spirit"
+        colorVariant={FlightTicketColorVariant.RED}
+        isLastElement={true}
       />
     </div>
   );
