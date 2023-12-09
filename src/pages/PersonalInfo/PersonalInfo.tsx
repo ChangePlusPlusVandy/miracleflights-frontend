697957cc84e7d9e44baf525d5176957460c403aa
jacoblurie29 <jacoblurie29@gmail.com>
2023-12-08 23:20:49 -0600

    Added memoization for performance

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,6 +1,6 @@
 import styles from "./PersonalInfo.module.css";
 import { createTestPassengerData } from "../../util/test-data.util";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import type { PassengerData } from "../../interfaces/passenger.interface";
 
 const PersonalInfo = () => {
@@ -8,36 +8,48 @@ const PersonalInfo = () => {
   const tabs = ["General", "Medical", "Financial", "Flight History"];
   const data: PassengerData = createTestPassengerData();
 
-  const generalInfo = {
-    "Full Name": data.fields["Full Name"],
-    Street: data.fields.Street,
-    Country: data.fields.Country,
-    "Email Address": data.fields.Email,
-    "Type of Passenger": data.fields.Type,
-    "Military Service Status": data.fields["Military Service"],
-    "Military Member Status": data.fields["Military Member"],
-    "Date Created": data.createdTime.split("T")[0],
-    "How did you hear about us?": data.fields["How did you hear about us"],
-  };
+  // Memoize the flight info so that it doesn't have to be recalculated every time the component re-renders
+  const flightInfo = useMemo(() => {
+    const generalInfo = {
+      "Full Name": data.fields["Full Name"],
+      Street: data.fields.Street,
+      Country: data.fields.Country,
+      "Email Address": data.fields.Email,
+      "Type of Passenger": data.fields.Type,
+      "Military Service Status": data.fields["Military Service"],
+      "Military Member Status": data.fields["Military Member"],
+      "Date Created": data.createdTime.split("T")[0],
+      "How did you hear about us?": data.fields["How did you hear about us"],
+    };
 
-  const medical = {
-    "Date of Birth": data.fields["Date of Birth"].split("T")[0],
-    Gender: data.fields.Gender,
-    Ethnicity: data.fields.Ethnicity,
-    Diagnosis: data.fields.Diagnosis,
-  };
+    const medicalInfo = {
+      "Date of Birth": data.fields["Date of Birth"].split("T")[0],
+      Gender: data.fields.Gender,
+      Ethnicity: data.fields.Ethnicity,
+      Diagnosis: data.fields.Diagnosis,
+    };
 
-  const financial = {
-    "Household Income": data.fields["Household Income"],
-    "Household Size": data.fields["Household Size"],
-  };
+    const financialInfo = {
+      "Household Income": data.fields["Household Income"],
+      "Household Size": data.fields["Household Size"],
+    };
 
-  const flightInfo = {
-    "All Flights": data.fields["All Flight Legs"],
-    "Number of Flight Legs": data.fields["# of Flight Legs"],
-    "Number of Booked Flight Requests":
-      data.fields["# of Booked Flight Requests (Patient)"],
-  };
+    const flightInfo = {
+      "All Flights": data.fields["All Flight Legs"],
+      "Number of Flight Legs": data.fields["# of Flight Legs"],
+      "Number of Booked Flight Requests":
+        data.fields["# of Booked Flight Requests (Patient)"],
+    };
+
+    return {
+      generalInfo,
+      medicalInfo,
+      financialInfo,
+      flightInfo,
+    };
+  }, [data]);
+
+  const { generalInfo, medicalInfo, financialInfo } = flightInfo;
 
   return (
     <div className={styles.tabs}>
@@ -60,34 +72,36 @@ const PersonalInfo = () => {
           <div key={0}>
             {Object.entries(generalInfo).map(([field, val], index: number) => (
               <div key={index} className={styles.InfoBox}>
-                {field}: {val}
+                {`${field}: ${val}`}
               </div>
             ))}
           </div>
         )}
         {tabIndex === 1 && (
           <div key={1}>
-            {Object.entries(medical).map(([field, val], index: number) => (
+            {Object.entries(medicalInfo).map(([field, val], index: number) => (
               <div key={index} className={styles.InfoBox}>
-                {field}: {val}
+                {`${field}: ${val}`}
               </div>
             ))}
           </div>
         )}
         {tabIndex === 2 && (
           <div key={2}>
-            {Object.entries(financial).map(([field, val], index: number) => (
-              <div key={index} className={styles.InfoBox}>
-                {field}: {val}
-              </div>
-            ))}
+            {Object.entries(financialInfo).map(
+              ([field, val], index: number) => (
+                <div key={index} className={styles.InfoBox}>
+                  {`${field}: ${val}`}
+                </div>
+              ),
+            )}
           </div>
         )}
         {tabIndex === 3 && (
           <div key={3}>
             {Object.entries(flightInfo).map(([field, val], index: number) => (
               <div key={index} className={styles.InfoBox}>
-                {field}: {val}
+                {`${field}: ${val}`}
               </div>
             ))}
           </div>
