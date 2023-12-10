e093b362120d66888781c54b4e6227c4fe89a6c1
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2023-12-10 15:42:37 -0600

    adding tests

---
+++ src/components/SelectComponent/__tests__/SelectComponent.test.tsx
@@ -0,0 +1,22 @@
+// Your test file
+import Select from "../SelectComponent";
+import { render } from "@testing-library/react";
+
+describe("SelectComponent Tests", () => {
+  test("Test Select component to render", () => {
+    const options = ["Option 1", "Option 2", "Option 3"];
+
+    // Mock register function
+    const mockRegister = jest.fn();
+
+    render(
+      <Select
+        name="testSelect"
+        register={mockRegister}
+        label="Select Label"
+        placeholder="Select Placeholder"
+        options={options}
+      />,
+    );
+  });
+});
