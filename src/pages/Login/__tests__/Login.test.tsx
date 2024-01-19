d01ab844ee5117f01017e8721e3827b3fff41ced
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-01-19 13:13:09 -0600

    Update Login.test.tsx
    Adding tests

---
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -0,0 +1,61 @@
+import Login from "../Login";
+import { render, fireEvent } from "@testing-library/react";
+
+describe("Login Tests", () => {
+  it("renders Login component correctly", () => {
+    const { getByText, getAllByPlaceholderText } = render(<Login />);
+
+    expect(getByText("Log In")).toBeTruthy();
+    expect(getByText("Dont have an account?")).toBeTruthy();
+    expect(
+      getAllByPlaceholderText("Email")[0] as HTMLInputElement,
+    ).toBeTruthy();
+    expect(
+      getAllByPlaceholderText("Password")[0] as HTMLInputElement,
+    ).toBeTruthy();
+  });
+
+  it("updates email and password on input change", () => {
+    const { getAllByPlaceholderText } = render(<Login />);
+    const emailInput = getAllByPlaceholderText("Email")[0] as HTMLInputElement;
+    const passwordInput = getAllByPlaceholderText(
+      "Password",
+    )[0] as HTMLInputElement;
+
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+
+    expect(emailInput.value).toBe("test@example.com");
+    expect(passwordInput.value).toBe("password123");
+  });
+
+  it("toggles password visibility", () => {
+    const { getByTestId } = render(<Login />);
+    const passwordToggle = getByTestId("passwordToggle");
+
+    fireEvent.click(passwordToggle);
+
+    expect(passwordToggle).toBeTruthy();
+  });
+
+  it("disables submit button when email or password is empty", () => {
+    const { getByText, getByPlaceholderText } = render(<Login />);
+    const submitButton = getByText("Login") as HTMLButtonElement;
+
+    fireEvent.click(submitButton);
+
+    // Ensure that the submit button is initially disabled
+    expect(submitButton.disabled).toBe(true);
+
+    // Fill in email and password
+    fireEvent.change(getByPlaceholderText("Email"), {
+      target: { value: "test@example.com" },
+    });
+    fireEvent.change(getByPlaceholderText("Password"), {
+      target: { value: "password123" },
+    });
+
+    // Ensure that the submit button is enabled after filling in email and password
+    expect(submitButton.disabled).toBe(false);
+  });
+});
