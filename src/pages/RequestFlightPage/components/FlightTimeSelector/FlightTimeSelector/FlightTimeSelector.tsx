9f0a005bde53be6e834cc5da0fbdd3294106a8f8
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-01-19 10:49:14 -0600

    APP-77-updating-flightSelector

--- src/pages/RequestFlightPage/components/FlightTimeSelector/FlightTimeSelector/FlightTimeSelector.tsx
+++ src/pages/RequestFlightPage/components/FlightTimeSelector/FlightTimeSelector/FlightTimeSelector.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import styles from "./FlightTimeSelector.module.css";
+// import "../../../../../styles/Calendar.css";
 import "react-calendar/dist/Calendar.css";
-import Icon from "../../../../components/CustomIcon/Icon.tsx";
+import Icon from "../../../../../components/CustomIcon/Icon.tsx";
 import DatePicker from "react-calendar";
 import { useState } from "react";
 import type { DateValue, FormTypes } from "./FlightTimeSelector.definitions.ts";
+// import { set } from "react-hook-form";
 
 const FlightTimeSelector = () => {
   const [departDate, setDepartDate] = useState<DateValue>(new Date());
@@ -48,64 +50,14 @@ const FlightTimeSelector = () => {
 
   return (
     <div>
-      <div className={styles.flightSelectorBlock}>
-        <div className={styles.flightSelectorBlockHeader}>Flight Selector</div>
+      <div className={styles.flightSelectorContainer}>
+        <div className={styles.flightSelectorBlockHeader}>Request a Flight</div>
         <div className={styles.flightSelectorBlockSubtitle}>
-          Select your flight
+          Choose two of the nearest airports to you and your destination.
         </div>
         <form className={styles.flightSelectorBlockContent}>
-          <div className={styles.flightSelectorInputContainerUpper}>
-            <div className={styles.airportChunk}>
-              <div className={styles.flightSelectorInputLabel}>
-                Departure Airport
-              </div>
-              <div className={styles.flightSelectorInputContainer}>
-                <input
-                  type="text"
-                  placeholder="Primary Selection"
-                  className={styles.flightSelectorInput}
-                  value={departureAirportPrimary}
-                  onChange={(e) => setDepartureAirportPrimary(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="Alternate Selection"
-                  className={styles.flightSelectorInput}
-                  value={departureAirportAlternate}
-                  onChange={(e) => setDepartureAirportAlternate(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className={styles.airplaneIcon}>
-              <Icon glyph="plane" />
-            </div>
-
-            <div className={styles.airportChunk}>
-              <div className={styles.flightSelectorInputLabel}>
-                Arrival Airport
-              </div>
-              <div className={styles.flightSelectorInputContainer}>
-                <input
-                  type="text"
-                  placeholder="Primary Selection"
-                  className={styles.flightSelectorInput}
-                  value={arrivalAirportPrimary}
-                  onChange={(e) => setArrivalAirportPrimary(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="Alternate Selection"
-                  className={styles.flightSelectorInput}
-                  value={arrivalAirportAlternate}
-                  onChange={(e) => setArrivalAirportAlternate(e.target.value)}
-                />
-              </div>
-            </div>
-          </div>
-
           <div className={styles.checkBoxContainer}>
-            <label className={styles.flightSelectorInputLabel}>One way:</label>
+            {/* <label className={styles.flightSelectorInputLabel}>One way:</label>
             <input
               type="checkbox"
               id="oneWay"
@@ -113,7 +65,73 @@ const FlightTimeSelector = () => {
               checked={oneWay}
               data-testid="oneWay"
               onChange={() => setOneWay(!oneWay)}
-            />{" "}
+            />{" "} */}
+            <div className={styles.toggleContainer}>
+              <div
+                className={`${styles.toggleItem} ${
+                  !oneWay ? styles.selected : ""
+                }`}
+                onClick={() => setOneWay(!oneWay)}
+              >
+                Round Trip
+              </div>
+              <div
+                className={`${styles.toggleItem} ${
+                  oneWay ? styles.selected : ""
+                }`}
+                onClick={() => setOneWay(!oneWay)}
+              >
+                One Way
+              </div>
+            </div>
+          </div>
+          <div className={styles.flightSelectorInputContainerUpper}>
+            <div className={styles.airportChunk}>
+              <div className={styles.flightSelectorInputLabelBlue}>
+                Departure
+              </div>
+              <div className={styles.flightSelectorInputContainer}>
+                <input
+                  type="text"
+                  placeholder="Primary Selection"
+                  className={styles.flightSelectorInputBlue}
+                  value={departureAirportPrimary}
+                  onChange={(e) => setDepartureAirportPrimary(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Alternate Selection"
+                  className={styles.flightSelectorInputBlueBorder}
+                  value={departureAirportAlternate}
+                  onChange={(e) => setDepartureAirportAlternate(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className={styles.horizontalLineWithIcon}>
+              <div className={styles.airplaneIcon}>
+                <Icon glyph="plane" />
+              </div>
+            </div>
+
+            <div className={styles.airportChunk}>
+              <div className={styles.flightSelectorInputLabelRed}>Arrival</div>
+              <div className={styles.flightSelectorInputContainer}>
+                <input
+                  type="text"
+                  placeholder="Primary Selection"
+                  className={styles.flightSelectorInputRed}
+                  value={arrivalAirportPrimary}
+                  onChange={(e) => setArrivalAirportPrimary(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Alternate Selection"
+                  className={styles.flightSelectorInputRedBorder}
+                  value={arrivalAirportAlternate}
+                  onChange={(e) => setArrivalAirportAlternate(e.target.value)}
+                />
+              </div>
+            </div>
           </div>
 
           <div className={styles.flightSelectorInputContainerLower}>
