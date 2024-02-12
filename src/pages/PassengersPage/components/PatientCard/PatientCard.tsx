3a6f12e8ca4bc4cf9b0627ca668e460c829ee7ea
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-02-12 09:55:37 -0600

    passenger details modal

--- src/pages/PassengersPage/components/PatientCard/PatientCard.tsx
+++ src/pages/PassengersPage/components/PatientCard/PatientCard.tsx
@@ -1,8 +1,7 @@
 import styles from "./PatientCard.module.css";
 import { formatDate, getAge } from "../../../../util/date.util";
-import PatientDetailsModal from "../PatientDetailsModal/PatientDetailsModal";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
+// import Icon from "../../../../components/CustomIcon/Icon";
+import Modal from "../../../../components/Modal/Modal";
 import { useState } from "react";
 import type { PatientProps } from "./PatientCard.definitions";
 
@@ -16,11 +15,72 @@ import type { PatientProps } from "./PatientCard.definitions";
  * @property {string} dateOfBirth
  */
 const Patient = ({ patient }: PatientProps) => {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modal, setModal] = useState(false);
 
   return (
     <>
-      <div className={styles.patientCard} onClick={() => setModalOpen(true)}>
+      {modal && (
+        <Modal
+          body={
+            <>
+              <div className={`${styles.patientRow} ${styles.marginBottom}`}>
+                <div>
+                  <span className={styles.patientLabel}>Gender</span>{" "}
+                  <span className={styles.patientText}>
+                    {patient.fields["Gender"]}
+                  </span>
+                </div>
+                <div>
+                  <span className={styles.patientLabel}>DOB</span>{" "}
+                  <span className={styles.patientText}>
+                    {patient.fields["Date of Birth"].split("T")[0]}{" "}
+                  </span>
+                </div>
+              </div>
+              {/* make another patient group for address where each line of the address is separated */}
+              <div className={styles.patientGroup}>
+                <span className={styles.patientLabel}>Address</span>{" "}
+                <span className={styles.patientText}>
+                  {patient.fields["Street"]}
+                </span>
+                <span className={styles.patientText}>
+                  {patient.fields["Country"]}
+                </span>
+              </div>
+              <div className={styles.patientGroup}>
+                <span className={styles.patientLabel}>Military</span>{" "}
+                <span className={styles.patientText}>
+                  {patient.fields["Military Service"]}
+                </span>
+              </div>
+              <div className={styles.patientRow}>
+                <div className={styles.patientGroup}>
+                  <span className={styles.patientLabel}># of Flight Legs</span>{" "}
+                  <span className={styles.patientText}>
+                    {patient.fields["# of Flight Legs"]}
+                  </span>
+                </div>
+                <div className={styles.patientGroup}>
+                  <span className={styles.patientLabel}>
+                    # of Booked Flight Requests
+                  </span>{" "}
+                  <span className={styles.patientText}>
+                    {patient.fields["# of Booked Flight Requests (Patient)"]}
+                  </span>
+                </div>
+              </div>
+              <div className={styles.patientGroup}>
+                <span className={styles.patientLabel}>Notes</span>
+                <span className={styles.patientText}>Notes go here</span>
+              </div>
+              {/* <span className={styles.patientLabel}>Joined Goes Here</span>{" "} */}
+            </>
+          }
+          header={patient.fields["Full Name"]}
+          action={() => setModal(false)}
+        />
+      )}
+      <div className={styles.patientCard} onClick={() => setModal(true)}>
         <div className={styles.imgAndInfo}>
           <div className={styles.imgContainer} />
           <div className={styles.info}>
@@ -41,16 +101,10 @@ const Patient = ({ patient }: PatientProps) => {
             </div>
           </div>
         </div>
-        <div className={styles.editIcon}>
-          <FontAwesomeIcon icon={faPen} />
-        </div>
+        {/* <div className={styles.editIcon} onClick={}>
+          <Icon glyph="pen" />
+        </div> */}
       </div>
-      {modalOpen && (
-        <PatientDetailsModal
-          patient={patient}
-          onClose={() => setModalOpen(false)}
-        />
-      )}
     </>
   );
 };
