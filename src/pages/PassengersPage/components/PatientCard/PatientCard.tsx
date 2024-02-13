e11226ae6caa193c2a6fff9b7844ea5f51f77c91
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-02-13 13:42:34 -0600

    making a patientcard modal

--- src/pages/PassengersPage/components/PatientCard/PatientCard.tsx
+++ src/pages/PassengersPage/components/PatientCard/PatientCard.tsx
@@ -1,7 +1,7 @@
 import styles from "./PatientCard.module.css";
 import { formatDate, getAge } from "../../../../util/date.util";
 // import Icon from "../../../../components/CustomIcon/Icon";
-import Modal from "../../../../components/Modal/Modal";
+import PatientDetailsModal from "../PatientDetailsModal/PatientDetailsModal";
 import { useState } from "react";
 import type { PatientProps } from "./PatientCard.definitions";
 
@@ -20,64 +20,9 @@ const Patient = ({ patient }: PatientProps) => {
   return (
     <>
       {modal && (
-        <Modal
-          body={
-            <>
-              <div className={`${styles.patientRow} ${styles.marginBottom}`}>
-                <div>
-                  <span className={styles.patientLabel}>Gender</span>{" "}
-                  <span className={styles.patientText}>
-                    {patient.fields["Gender"]}
-                  </span>
-                </div>
-                <div>
-                  <span className={styles.patientLabel}>DOB</span>{" "}
-                  <span className={styles.patientText}>
-                    {patient.fields["Date of Birth"].split("T")[0]}{" "}
-                  </span>
-                </div>
-              </div>
-              {/* make another patient group for address where each line of the address is separated */}
-              <div className={styles.patientGroup}>
-                <span className={styles.patientLabel}>Address</span>{" "}
-                <span className={styles.patientText}>
-                  {patient.fields["Street"]}
-                </span>
-                <span className={styles.patientText}>
-                  {patient.fields["Country"]}
-                </span>
-              </div>
-              <div className={styles.patientGroup}>
-                <span className={styles.patientLabel}>Military</span>{" "}
-                <span className={styles.patientText}>
-                  {patient.fields["Military Service"]}
-                </span>
-              </div>
-              <div className={styles.patientRow}>
-                <div className={styles.patientGroup}>
-                  <span className={styles.patientLabel}># of Flight Legs</span>{" "}
-                  <span className={styles.patientText}>
-                    {patient.fields["# of Flight Legs"]}
-                  </span>
-                </div>
-                <div className={styles.patientGroup}>
-                  <span className={styles.patientLabel}>
-                    # of Booked Flight Requests
-                  </span>{" "}
-                  <span className={styles.patientText}>
-                    {patient.fields["# of Booked Flight Requests (Patient)"]}
-                  </span>
-                </div>
-              </div>
-              <div className={styles.patientGroup}>
-                <span className={styles.patientLabel}>Notes</span>
-                <span className={styles.patientText}>Notes go here</span>
-              </div>
-              {/* <span className={styles.patientLabel}>Joined Goes Here</span>{" "} */}
-            </>
-          }
-          header={patient.fields["Full Name"]}
-          action={() => setModal(false)}
+        <PatientDetailsModal
+          patient={patient}
+          onClose={() => setModal(false)}
         />
       )}
       <div className={styles.patientCard} onClick={() => setModal(true)}>
