b81d1895b5378943b028503898256787df464822
jacoblurie29 <jacoblurie29@gmail.com>
2024-03-22 09:57:14 -0500

    React query and provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import Router from "./layout/Router.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
