ff0ae1faa05ef6d5a64070cb57dbba67b4868955
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-04-12 12:02:47 -0500

    Adding picture rotundra

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,4 +1,5 @@
 import styles from "./DashboardPage.module.css";
+import Rotunda from "./components/Rotunda/Rotunda";
 import { useNavigationContext } from "../../context/Navigation.context";
 import { Tabs } from "../../layout/SideBar/SideBar.definitions";
 import { getDashboardData } from "../../api/queries";
@@ -10,6 +11,13 @@ import type { DashboardData } from "./DashboardPage.definitions";
 const DashboardPage = () => {
   const { setCurrentTab } = useNavigationContext();
   const { getToken } = useAuth();
+
+  const images = [
+    "src/pages/DashboardPage/components/DJI_0632.jpeg",
+    "src/pages/DashboardPage/components/DJI_20230609100419_0001_V.jpeg",
+    "src/pages/DashboardPage/components/Campus_aerial_fall.jpg",
+    "src/pages/DashboardPage/components/Image 4-5-22 at 1.45 PM.jpg",
+  ];
 
   const { data: dashboardData, isLoading: dashboardLoading } =
     useQuery<DashboardData>({
@@ -28,26 +36,33 @@ const DashboardPage = () => {
 
   return (
     <>
-      <div className={styles.welcomeMessage}>
-        <div className={styles.cardStyle}>
-          <div className={styles.flightNumber}>
-            {dashboardData?.["Flights Today"]}
-          </div>
-          <div className={styles.flightsToday}>FLIGHTS TODAY</div>
+      <div className={styles.dashboardContainer}>
+        <div className={styles.dashboardTitle}>
+          Welcome back to Miracle Flights
         </div>
-        <div className={styles.cardStyle}>
-          <div className={styles.flightNumber}>
-            {(Number(dashboardData?.["Flights This Week"]) || 0) +
-              (Number(dashboardData?.["Flights Today"]) || 0)}
+        <div className={styles.CardsContainer}>
+          <div className={styles.cardStyle}>
+            <div className={styles.flightNumber}>
+              {dashboardData?.["Flights Today"]}
+            </div>
+            <div className={styles.flightsToday}>FLIGHTS TODAY</div>
           </div>
-          <div className={styles.flightsWeek}>FLIGHTS THIS WEEK </div>
-        </div>
-        <div className={styles.cardStyle}>
-          <div className={styles.flightNumber}>
-            {dashboardData?.["All Total Flights"]}
+          <div className={styles.cardStyle}>
+            <div className={styles.flightNumber}>
+              {(Number(dashboardData?.["Flights This Week"]) || 0) +
+                (Number(dashboardData?.["Flights Today"]) || 0)}
+            </div>
+            <div className={styles.flightsWeek}>FLIGHTS THIS WEEK </div>
           </div>
-          <div className={styles.flightsEver}>FLIGHTS EVER</div>
+          <div className={styles.cardStyle}>
+            <div className={styles.flightNumber}>
+              {dashboardData?.["All Total Flights"]}
+            </div>
+            <div className={styles.flightsEver}>FLIGHTS TOTAL</div>
+          </div>
         </div>
+        <div className={styles.dashboardTitle}>Miracle Flights Pictures</div>
+        <Rotunda images={images} />
       </div>
     </>
   );
