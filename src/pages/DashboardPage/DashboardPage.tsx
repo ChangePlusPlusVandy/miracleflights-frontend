08a1b90be3abc0ffbd4ee898ebb82453a186b3dd
jacoblurie29 <jacoblurie29@gmail.com>
2024-04-15 15:25:33 -0500

    Final changes to dashboard

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -3,20 +3,30 @@ import Rotunda from "./components/Rotunda/Rotunda";
 import { useNavigationContext } from "../../context/Navigation.context";
 import { Tabs } from "../../layout/SideBar/SideBar.definitions";
 import { getDashboardData } from "../../api/queries";
+import Divider from "../../components/Divider/Divider";
+import { DividerSpacing } from "../../components/Divider/Divider.definitions";
 import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 import type { DashboardData } from "./DashboardPage.definitions";
 
 const DashboardPage = () => {
   const { setCurrentTab } = useNavigationContext();
   const { getToken } = useAuth();
+  const { user } = useUser();
+  const navigate = useNavigate();
 
   const images = [
-    "src/pages/DashboardPage/components/DJI_0632.jpeg",
-    "src/pages/DashboardPage/components/DJI_20230609100419_0001_V.jpeg",
-    "src/pages/DashboardPage/components/Campus_aerial_fall.jpg",
-    "src/pages/DashboardPage/components/Image 4-5-22 at 1.45 PM.jpg",
+    "src/pages/DashboardPage/components/MF_1.jpeg",
+    "src/pages/DashboardPage/components/MF_2.jpeg",
+    "src/pages/DashboardPage/components/MF_3.jpeg",
+    "src/pages/DashboardPage/components/MF_4.jpeg",
+    "src/pages/DashboardPage/components/MF_5.jpeg",
+    "src/pages/DashboardPage/components/MF_7.png",
+    "src/pages/DashboardPage/components/MF_8.webp",
   ];
 
   const { data: dashboardData, isLoading: dashboardLoading } =
@@ -38,8 +48,9 @@ const DashboardPage = () => {
     <>
       <div className={styles.dashboardContainer}>
         <div className={styles.dashboardTitle}>
-          Welcome back to Miracle Flights
+          Welcome back, {user?.firstName}!
         </div>
+        <Divider spacing={DividerSpacing.LARGE} />
         <div className={styles.CardsContainer}>
           <div className={styles.cardStyle}>
             <div className={styles.flightNumber}>
@@ -61,8 +72,42 @@ const DashboardPage = () => {
             <div className={styles.flightsEver}>FLIGHTS TOTAL</div>
           </div>
         </div>
-        <div className={styles.dashboardTitle}>Miracle Flights Pictures</div>
+        <Divider spacing={DividerSpacing.LARGE} />
+        <h4>Take a look into Miracle Flights: </h4>
         <Rotunda images={images} />
+        <Divider spacing={DividerSpacing.LARGE} />
+        <div className={styles.CardsContainer}>
+          <div
+            className={styles.quickLinkContainer}
+            onClick={() => navigate("/request")}
+          >
+            <div className={styles.quickLinkText}>REQUEST FLIGHT</div>
+            <FontAwesomeIcon
+              className={styles.quickLinkText}
+              icon={faArrowUpRightFromSquare}
+            />
+          </div>
+          <div
+            className={styles.quickLinkContainer}
+            onClick={() => navigate("/passengers")}
+          >
+            <div className={styles.quickLinkText}>PASSENGERS</div>
+            <FontAwesomeIcon
+              className={styles.quickLinkText}
+              icon={faArrowUpRightFromSquare}
+            />
+          </div>
+          <div
+            className={styles.quickLinkContainer}
+            onClick={() => navigate("/trips")}
+          >
+            <div className={styles.quickLinkText}>YOUR TRIPS</div>
+            <FontAwesomeIcon
+              className={styles.quickLinkText}
+              icon={faArrowUpRightFromSquare}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
