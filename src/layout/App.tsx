151af79a4e0b21ddf14692f45020fc2128c68ead
zszul <150448992+zszul@users.noreply.github.com>
2025-01-07 20:49:46 -0600

    Notifications frontend
    git rebase --continue
    Notifications frontend

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -2,7 +2,7 @@ import styles from "./App.module.css";
 import SideBar from "./SideBar/Sidebar";
 import { getUserByAirtableRecordId } from "../api/queries";
 import { useUserContext } from "../context/User.context";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { useUser, useAuth } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
@@ -11,8 +11,11 @@ import type { PassengerData } from "../interfaces/passenger.interface";
 const App = () => {
   const { user } = useUser();
   const navigate = useNavigate();
+  const location = useLocation();
   const { getToken } = useAuth();
   const { currentUser, setCurrentUser } = useUserContext();
+
+  const isNotificationsPage = location.pathname === "/notifications";
 
   const { data: userData, error } = useQuery({
     queryKey: ["user", user?.id],
@@ -27,29 +30,35 @@ const App = () => {
 
   // if there is an airtable record id, but no user data, get the user
   useEffect(() => {
+    if (error && !currentUser) {
+      navigate("/onboard");
+    }
+  }, [user, userData]);
+
+  useEffect(() => {
     if (userData) {
       setCurrentUser(userData as PassengerData);
     }
   }, [userData]);
 
-  useEffect(() => {
-    if (!user?.publicMetadata.airtableRecordId) {
-      navigate("/onboard");
-    }
-  }, [currentUser]);
-
-  /*useEffect(() => {
-    if (error && !currentUser) {
-      navigate("/onboard");
-    }
-  }, [user, userData]);*/
-
   return (
-    <div className={styles.appContainer}>
+    <div
+      className={`${styles.appContainer} ${
+        isNotificationsPage ? styles.notificationsPage : ""
+      }`}
+    >
       <div className={styles.sideBarContainer}>
         <SideBar />
       </div>
-      <div className={styles.contentContainer}>
+      <div
+        className={styles.contentContainer}
+        style={{
+          background: isNotificationsPage
+            ? "linear-gradient(to bottom, #d3ddf0, #f1f5fb)"
+            : "#ffffff",
+          padding: isNotificationsPage ? "0" : "4rem 6rem",
+        }}
+      >
         {!currentUser ? <div>Loading...</div> : <Outlet />}
       </div>
     </div>
