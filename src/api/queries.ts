c13d198c66c029fb6dea847aeeef0d6023506a85
mseok <mseok.son@gmail.com>
2025-01-25 13:04:24 -0600

    fixed navigation issues

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import type { FlightRequestData } from "../interfaces/flight-request-interface";
 import type { DashboardData } from "../pages/DashboardPage/DashboardPage.definitions";
 import type { PassengerData } from "../interfaces/passenger.interface";
+import type { DocumentsData } from "../pages/DocumentsPage/Documents.definitions";
 
 export const getPassengers = async (
   id: string,
@@ -208,6 +209,17 @@ export const getDashboardData = (
 ): Promise<DashboardData> =>
   axios
     .get(`${process.env.VITE_HOST}/dashboard`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => res.data);
+
+export const getDocumentsData = (
+  token?: string | null,
+): Promise<DocumentsData> =>
+  axios
+    .get(`${process.env.VITE_HOST}/documents`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
