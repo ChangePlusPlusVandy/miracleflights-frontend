a6897f5a2b0ffbc7541b498ba0ebdcfd2ea2e7ec
mseok <mseok.son@gmail.com>
2025-03-26 14:57:29 -0500

    remove comments

--- src/pages/PassengersPage/PassengersPage.tsx
+++ src/pages/PassengersPage/PassengersPage.tsx
@@ -1,101 +1,3 @@
-// import styles from "./PassengersPage.module.css";
-// import PatientCard from "./components/PatientCard/PatientCard";
-// import PassengerCard from "./components/PassengerCard/PassengerCard";
-// import PlusButton from "./components/PlusButton/PlusButton";
-
-// import { getAccompanyingPassengers, getPassengers } from "../../api/queries";
-// import { useUserContext } from "../../context/User.context";
-// import { useNavigationContext } from "../../context/Navigation.context";
-// import { Tabs } from "../../layout/SideBar/SideBar.definitions";
-// import { useQuery } from "@tanstack/react-query";
-// import { useAuth } from "@clerk/clerk-react";
-// import { useEffect, useState } from "react";
-// import type { PassengerData } from "../../interfaces/passenger.interface";
-
-// const PassengersPage = () => {
-//   const { getToken } = useAuth();
-//   const { currentUser } = useUserContext();
-//   const { setCurrentTab } = useNavigationContext();
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   // ping the getUserByID endpoint to get the user's data
-//   const { data: passengerData, isLoading: passengerLoading } =
-//     useQuery<PassengerData>({
-//       queryKey: ["passenger"],
-//       queryFn: async () =>
-//         getPassengers(
-//           currentUser?.["AirTable Record ID"] || "",
-//           await getToken(),
-//         ),
-//       enabled: true,
-//     });
-
-// const {
-//   data: accompanyingPassengerData,
-//   isLoading: accompanyingPassengerLoading,
-// } = useQuery<PassengerData[]>({
-//   queryKey: ["accompanyingPassengers"],
-//   queryFn: async () =>
-//     getAccompanyingPassengers(
-//       currentUser?.["AirTable Record ID"] || "",
-//       await getToken(),
-//     ),
-//   enabled: true,
-// });
-
-//   useEffect(() => {
-//     setCurrentTab(Tabs.PASSENGERS);
-//   }, []);
-
-//   if (
-//     passengerLoading ||
-//     accompanyingPassengerLoading ||
-//     !accompanyingPassengerData ||
-//     !passengerData
-//   ) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <>
-//       {isModalOpen && (
-//         <AddPassengerModal onClose={() => setIsModalOpen(false)} />
-//       )}
-//       <div className={styles.container}>
-//         <div className={styles.patientSection}>
-//           <h2 className={styles.header}>Patients and Companions</h2>
-//           <h3 className={styles.subheader}>Patient Information</h3>
-//           <div className={styles.patientInfo}>
-//             <PatientCard patient={passengerData} />
-//           </div>
-//         </div>
-//         <div className={styles.passengerSection}>
-//           <div className={styles.passengerSectionHeader}>
-//             <h3 className={styles.subheader}>
-//               Companion Information
-//               <div className={styles.plusButton}>
-//                 <PlusButton setOpen={setIsModalOpen} />
-//               </div>{" "}
-//             </h3>
-//           </div>
-//           <h5 className={styles.description}>
-//             Companions of {passengerData["First Name"]}
-//           </h5>
-//           <div className={styles.passengersContainer}>
-//             {accompanyingPassengerData.map((passenger, index) => (
-//               <div className={styles.passengerCard} key={index}>
-//                 <PassengerCard passenger={passenger} />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default PassengersPage;
-
 import styles from "./PassengersPage.module.css";
 import PatientCard from "./components/PatientCard/PatientCard";
 import PassengerCard from "./components/PassengerCard/PassengerCard";
@@ -151,9 +53,6 @@ const PassengersPage = () => {
   console.log(patientData);
   return (
     <div className={styles.container}>
-      {/* Fixed header outside the scroll container */}
-      {/* <div className={styles.fixedHeader}></div> */}
-
       <h2 className={styles.header}>Patient and Companions</h2>
       <p className={styles.description}>
         Here, you can access all related patient data and companion information.
@@ -178,13 +77,6 @@ const PassengersPage = () => {
       <p className={styles.companionDescription}>
         {patientData["First Name"]}'s companions
       </p>
-      {/* <div className={styles.companionsGrid}>
-        {companionsData.map((companion, index) => (
-          <div className={styles.companionCard} key={index}>
-            <CompanionCard companion={companion} />
-          </div>
-        ))}
-      </div> */}
 
       <div className={styles.passengersContainer}>
         {companionsData.map((passenger, index) => (
@@ -193,17 +85,6 @@ const PassengersPage = () => {
           </div>
         ))}
       </div>
-
-      {/* <div className={styles.section}>
-          
-        </div> */}
-
-      {/* <div className={styles.requestTripContainer}>
-        <button className={styles.requestTripButton}>Request a Trip</button>
-      </div> */}
-
-      {/* Scrollable content container */}
-      {/* <div className={styles.scrollContainer}></div> */}
 
       {isModalOpen && (
         <AddPassengerModal onClose={() => setIsModalOpen(false)} />
