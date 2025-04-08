49802d1f1381c909b439db034723af8f94eaac61
mseok <mseok.son@gmail.com>
2025-04-08 02:23:05 -0500

    remove unnecessary files

--- src/pages/PassengersPage/components/TestModal/PassengerDetailsModal.definitions.ts
+++
@@ -1,6 +0,0 @@
-import type { PassengerData } from "../../../../interfaces/passenger.interface";
-
-export interface PassengerDetailsModalProps {
-  passenger: PassengerData;
-  onClose: () => void;
-}
